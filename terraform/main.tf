78838bf7c48e53a2b52a90df288af20e6e7478a5
moabukar <mo-abukar10@hotmail.com>
2023-12-04 17:11:42 +0000

    update mod region

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,15 @@ module "network" {
   vpc_cidr_block = "10.0.0.0/16"
 
   public_subnets = {
-    "ap-southeast-1a" = "10.0.1.0/24"
-    "ap-southeast-1b" = "10.0.2.0/24"
-    "ap-southeast-1c" = "10.0.3.0/24"
+    "eu-west-1a" = "10.0.1.0/24"
+    "eu-west-1b" = "10.0.2.0/24"
+    "eu-west-1c" = "10.0.3.0/24"
   }
 
   private_subnets = {
-    "ap-southeast-1a" = "10.0.11.0/24"
-    "ap-southeast-1b" = "10.0.12.0/24"
-    "ap-southeast-1c" = "10.0.13.0/24"
+    "eu-west-1a" = "10.0.11.0/24"
+    "eu-west-1b" = "10.0.12.0/24"
+    "eu-west-1c" = "10.0.13.0/24"
   }
 }
 
